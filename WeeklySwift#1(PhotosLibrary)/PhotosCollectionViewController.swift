3c5ae902689d87768ac30dd8d5173c1569c3e7e2
Ruslan <ruslanfilistovich@gmail.com>
2019-08-24 21:59:51 +0300

    add setupNavigationBar(), setupCollectionView()

---
+++ WeeklySwift#1(PhotosLibrary)/PhotosCollectionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PhotosCollectionViewController.swift
+//  WeeklySwift#1(PhotosLibrary)
+//
+//  Created by Ruslan on 8/24/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .orange
+        setupNavigationBar()
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellId")
+    }
+    
+    private func setupNavigationBar() {
+        let titleLabel = UILabel()
+        titleLabel.text = "PHOTOS"
+        titleLabel.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        titleLabel.textColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellId", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+
+}
