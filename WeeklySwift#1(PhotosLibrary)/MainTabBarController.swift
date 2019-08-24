9d6c9a9f9a5111b99b9f08fb2de175a08f1232b0
Ruslan <ruslanfilistovich@gmail.com>
2019-08-24 21:35:33 +0300

    add icons, navigation

---
+++ WeeklySwift#1(PhotosLibrary)/MainTabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  MainTabBarController.swift
+//  WeeklySwift#1(PhotosLibrary)
+//
+//  Created by Ruslan on 8/24/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        let photosVC = PhotosCollectionViewController(collectionViewLayout: UICollectionViewFlowLayout())
+                
+        viewControllers = [
+           
+            generateNavigationController(rootViewController: photosVC, title: "Photos", image: #imageLiteral(resourceName: "photos")),
+            generateNavigationController(rootViewController: ViewController(), title: "Favourites", image: #imageLiteral(resourceName: "heart"))
+        ]
+    }
+    
+    private func generateNavigationController(rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = image
+        return navigationVC
+    }
+}
