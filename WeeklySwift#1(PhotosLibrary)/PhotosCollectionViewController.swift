a0c5a1f6d694a0ab2ff2f8df6632e312c90bf418
Ruslan <ruslanfilistovich@gmail.com>
2019-08-24 22:18:18 +0300

    add actionBarButton

--- WeeklySwift#1(PhotosLibrary)/PhotosCollectionViewController.swift
+++ WeeklySwift#1(PhotosLibrary)/PhotosCollectionViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    private lazy var addBarButtonItem: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: .add, target: self,
+                               action: #selector(addBarButtonTapped))
+    }()
+    
+    private lazy var actionBarButtonItem: UIBarButtonItem = {
+       return UIBarButtonItem(barButtonSystemItem: .action, target: self,
+                              action: #selector(actionBarButtonTapped))
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,6 +27,30 @@ class PhotosCollectionViewController: UICollectionViewController {
         setupNavigationBar()
         setupCollectionView()
     }
+    
+    // MARK: - NavigationItems action
+    @objc private func addBarButtonTapped() {
+        print(#function)
+    }
+    
+    @objc private func actionBarButtonTapped(sender: UIBarButtonItem) {
+        print(#function)
+        
+//        let shareController = UIActivityViewController(activityItems: selectedImages, applicationActivities: nil)
+//
+//
+//        shareController.completionWithItemsHandler = { _, bool, _, _ in
+//            if bool {
+//                self.refresh()
+//            }
+//        }
+//
+//        shareController.popoverPresentationController?.barButtonItem = sender
+//        shareController.popoverPresentationController?.permittedArrowDirections = .any
+//        present(shareController, animated: true, completion: nil)
+    }
+    
+    // MARK: - Setup UI Elements
     
     private func setupCollectionView() {
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellId")
@@ -28,7 +62,12 @@ class PhotosCollectionViewController: UICollectionViewController {
         titleLabel.font = UIFont.systemFont(ofSize: 15, weight: .medium)
         titleLabel.textColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
         navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
+        
+        navigationItem.rightBarButtonItems = [actionBarButtonItem, addBarButtonItem]
     }
+    
+    
+    // MARK: - UICollecionViewDataSource, UICollecionViewDelegate
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
